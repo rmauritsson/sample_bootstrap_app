759c6b3f49a878d7008fa817be5f366580a224f7
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-06 13:06:58 -0300

    rubocop fixed, no offenses detected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   helper Webpacker::Helper
 
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: 'hello, world!'
   end
 end
